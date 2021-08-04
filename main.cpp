de0126703d331836055481248782136d7c2f76fb
Sarmouts <saradis.m06@gmail.com>
2021-08-04 15:40:08 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Enter your name" << endl;
+    string USER_INPUT;
+    cin >> USER_INPUT;
+    cout << "Hello, " << USER_INPUT << endl;
+}
